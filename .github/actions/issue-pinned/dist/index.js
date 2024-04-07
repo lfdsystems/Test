9a5b29b284f15c5daf2d294b2968260259b48914
Vipin Kumar <developer.lfdsystems@outlook.com>
2024-04-07 09:45:58 +0530

    issue-pinned

--- .github/actions/issue-pinned/dist/index.js
+++ .github/actions/issue-pinned/dist/index.js
@@ -55873,46 +55873,10 @@ const { GraphQLClient } = __nccwpck_require__(5406)
  */
 async function run() {
   try {
-    const GHPAYLOAD = GITHUB.context.payload
-    const ISSUENODEID = GHPAYLOAD.issue.node_id
-    const issueId = ISSUENODEID
-    const GHTOKEN = CORE.getInput('GHTOKEN', { required: true })
-    console.log(`Token: ${GHTOKEN}`)
-    const commentBody = 'This is the new comment'
-    const client = new GraphQLClient('https://api.github.com/graphql', {
-      headers: { Authorization: `Bearer ${GHTOKEN}` }
-    })
-
-    let mutation = `
-      mutation UnpinIssue($issueId: ID!) {
-        unpinIssue(input: { issueId: $issueId} ) {
-          clientMutationId
-        }
-      }
-    `
-    let variables = { issueId }
-    let response = await client.request(mutation, variables)
-    console.log('Response:\n', response)
-
-    mutation = `
-      mutation AddCommentToIssue($issueId: ID!, $commentBody: String!){
-        addComment(input: { subjectId: $issueId, body: $commentBody }) {
-          commentEdge {
-            node {
-              id
-              body
-            }
-          }
-        }
-      }
-    `
-
-    variables = { issueId, commentBody }
-    response = await client.request(mutation, variables)
-    console.log('Response:\n', response)
+    throw new Error('Something went wrong')
   } catch (error) {
     // Fail the workflow step if an error occurs
-    CORE.setFailed(error.message)
+    CORE.error(error.message)
   }
 }
 
