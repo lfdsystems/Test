bc396333be4301602c50ef054e3c2fe771a4116b
Vipin Kumar <developer.lfdsystems@outlook.com>
2024-04-07 10:22:10 +0530

    issue-pinned

--- .github/actions/issue-pinned/src/main.js
+++ .github/actions/issue-pinned/src/main.js
@@ -1,6 +1,5 @@
 const CORE = require('@actions/core')
 const GITHUB = require('@actions/github')
-const { GraphQLClient } = require('graphql-request')
 
 /**
  * The main function for the issue-pinned.
@@ -8,24 +7,23 @@ const { GraphQLClient } = require('graphql-request')
  */
 async function run() {
   try {
-    CORE.info('\u001b[35mStarting the workflow...')
-    CORE.info('\u001b[38;5;6mTask completed successfully.')
-    CORE.info('\u001b[38;2;255;0;0mThis foreground will be bright red')
-    CORE.info('\u001b[43mThis background will be yellow')
-    CORE.info('\u001b[48;5;6mThis background will be cyan')
-    CORE.info('\u001b[48;2;255;0;0mThis background will be bright red')
-    CORE.info('\u001b[1mBold text')
-    CORE.info('\u001b[3mItalic text')
-    CORE.info('\u001b[4mUnderlined text')
-    CORE.info(
-      '\u001b[31;46mRed foreground with a cyan background and \u001b[1mbold text at the end'
-    )
-    CORE.info('\u001b[35mThis foreground will be magenta')
-    CORE.info('This foreground will reset to the default')
-    throw new Error('Error')
+    const token = CORE.getInput('GHTOKEN', { required: true })
+    const payload = GITHUB.context.payload
+    const issueId = payload.issue.node_id
+    const octokit = GITHUB.getOctokit(token)
+    const mutation = `
+      mutation UnpinIssue($issueId: ID!) {
+        unpinIssue(input: { issueId: $issueId }) {
+          clientMutationId
+        }
+      }
+    `
+    const variables = { issueId }
+    const result = await octokit.graphql(mutation, variables)
+    console.log(result)
   } catch (error) {
     // Fail the workflow step if an error occurs
-    CORE.setFailed(`\u001b[38;2;255;0;0m${error.message}`)
+    CORE.setFailed(error.message)
   }
 }
 
