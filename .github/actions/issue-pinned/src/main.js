4e12b276db02676cf71ca1dc9df1525eaa92fdce
Vipin Kumar <developer.lfdsystems@outlook.com>
2024-04-07 09:57:18 +0530

    issue-pinned

--- .github/actions/issue-pinned/src/main.js
+++ .github/actions/issue-pinned/src/main.js
@@ -8,9 +8,20 @@ const { GraphQLClient } = require('graphql-request')
  */
 async function run() {
   try {
-    CORE.info('Starting the workflow...')
-    // Code that performs certain tasks
-    CORE.info('Task completed successfully.')
+    CORE.info('\u001b[35mStarting the workflow...')
+    CORE.info('\u001b[38;5;6mTask completed successfully.')
+    CORE.info('\u001b[38;2;255;0;0mThis foreground will be bright red')
+    CORE.info('\u001b[43mThis background will be yellow')
+    CORE.info('\u001b[48;5;6mThis background will be cyan')
+    CORE.info('\u001b[48;2;255;0;0mThis background will be bright red')
+    CORE.info('\u001b[1mBold text')
+    CORE.info('\u001b[3mItalic text')
+    CORE.info('\u001b[4mUnderlined text')
+    CORE.info(
+      '\u001b[31;46mRed foreground with a cyan background and \u001b[1mbold text at the end'
+    )
+    CORE.info('\u001b[35mThis foreground will be magenta')
+    CORE.info('This foreground will reset to the default')
   } catch (error) {
     // Fail the workflow step if an error occurs
     CORE.setFailed(error.message)
